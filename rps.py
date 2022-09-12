2ee0f36f1f90094565b0ab56873f84ccb8e1dc73
mohamad.alnajjar <mohamad.alnajjar@elev.ga.ntig.se>
2022-09-12 13:28:48 +0200

    Rps finished

--- rps.py
+++ rps.py
@@ -1,9 +1,42 @@
 import random
+computer_score = 0
+user_score = 0
 
-computer = random.choice(["rock", "paper", "scissors"])
+while computer_score < 3 and user_score < 3:
+    computer = random.choice(["rock", "paper", "scissors"])
 
-user = input("rock, paper or scissors? ")
+    user = input("rock, paper or scissors? ")
 
-print("The computer chose", computer,"and the user chose", user +".")
+    print("The computer chose", computer,"and the user chose", user +".")
 
-# TODO - Implement the if statements that prints who wins
+    if computer == "rock" and user == "scissors":
+        computer_score += 1
+        print("Computer got a point ") 
+        print(computer_score, user_score)
+
+    elif computer == "paper" and user == "rock":
+        computer_score += 1
+        print("Computer got a point") 
+        print(computer_score, user_score)
+
+    elif computer == "scissors" and user == "rock":
+        computer_score += 1
+        print("Computer got a point") 
+        print(computer_score, user_score)
+
+    elif computer == "rock" and user == "paper":
+        user_score += 1
+        print("You got a point") 
+        print(computer_score, user_score)
+
+    elif computer == "scissors" and user == "rock":
+        user_score += 1
+        print("You got a point") 
+        print(computer_score, user_score)
+
+    elif computer == "paper" and user == "scissors":
+        user_score += 1
+        print("You got a point") 
+        print(computer_score, user_score)
+
+    # TODO - Implement the if statements that prints who wins
